051459af863b6688d6d51e0bb3c47940b1ef18ae
Joe Bonneau <joebonneau@gmail.com>
2023-11-26 17:04:26 -0800

    make some more layout modifications

--- keyboards/ferris/keymaps/joebonneau/config.h
+++ keyboards/ferris/keymaps/joebonneau/config.h
@@ -23,6 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 0
 
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
 // Auto Shift
 #define NO_AUTO_SHIFT_ALPHA
 #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
